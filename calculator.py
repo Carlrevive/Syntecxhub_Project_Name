c572ef7e770652ba1e9b689e942eb8f269d6577d
Carlos Antwi <carlosantwi5@gmail.com>
2025-10-18 03:22:07 +0000

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,20 +1,20 @@
 class calculator():
     def add():
-        if operand=="+":
-            return print(number1+number2)
+        if operator=="+":
+            return print(float(number1+number2))
        
     def subtract():
-        if operand=="-":
-           return print(number1-number2)
+        if operator=="-":
+           return print(float(number1-number2))
         
     def multiply():
-        if operand=="*":
-           return print(number1*number2)
+        if operator=="*":
+           return print(float(number1*number2))
         
     def divide():
      try:
-        if operand=="/":
-             print(number1/number2)
+        if operator=="/":
+             print(float(number1/number2))
      except:
         print("Zero Division Error")
 
@@ -23,7 +23,7 @@ class calculator():
 while True:
   
     number1 = int(input("Enter first number: "))
-    operand = input("Enter the operation sign (+,-,*,/): ")  
+    operator = input("Enter the operation sign (+,-,*,/): ")  
     number2 = int(input("Enter second number: "))
     calculator.add()  
     calculator.subtract()
@@ -32,6 +32,7 @@ while True:
     exit_continue=input("Press 'e' for exit or Enter key to continue calculation: ")
     if exit_continue:
        break
+
 
 
 
