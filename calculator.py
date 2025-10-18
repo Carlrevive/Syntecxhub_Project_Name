72687ef3a74af5e8ba2bfed95666e1465bdea4f4
Carlos Antwi <carlosantwi5@gmail.com>
2025-10-18 03:07:29 +0000

    Refactor calculator functions into a class

--- calculator.py
+++ calculator.py
@@ -1,38 +1,38 @@
-def add():
-    if operand=="+":
-        return print(number1+number2)
-   
-def subtract():
-    if operand=="-":
-       return print(number1-number2)
-    
-def multiply():
-    if operand=="*":
-       return print(number1*number2)
-    
-def divide():
- try:
-    if operand=="/":
-         print(number1/number2)
- except:
-    print("Zero Division Error")
+class calculator():
+    def add():
+        if operand=="+":
+            return print(number1+number2)
+       
+    def subtract():
+        if operand=="-":
+           return print(number1-number2)
+        
+    def multiply():
+        if operand=="*":
+           return print(number1*number2)
+        
+    def divide():
+     try:
+        if operand=="/":
+             print(number1/number2)
+     except:
+        print("Zero Division Error")
 
-def clear():
-   if clear:
-      clear
+
       
 while True:
   
     number1 = int(input("Enter first number: "))
     operand = input("Enter the operation sign (+,-,*,/): ")  
     number2 = int(input("Enter second number: "))
-    add()  
-    subtract()
-    multiply()
-    divide()
+    calculator.add()  
+    calculator.subtract()
+    calculator.multiply()
+    calculator.divide()
     exit_continue=input("Press 'e' for exit or Enter key to continue calculation: ")
     if exit_continue:
        break
+
 
 
 
